7135077eb4b6837207652ce9e85fbc2772f9c880
Bobby Schuchert <bobby.schuchert@sparcedge.com>
2013-10-25 15:04:52 -0400

    Initial Commit

---
+++ XcodeBotsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XcodeBotsTest
+//
+//  Created by Bobby Schuchert on 10/25/13.
+//  Copyright (c) 2013 SPARC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XcodeBotsTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XcodeBotsTest
+//
+//  Created by Bobby Schuchert on 10/25/13.
+//  Copyright (c) 2013 SPARC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
